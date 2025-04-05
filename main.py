38defa327069a619d7238efdd1d98f1fd2da2107
Luipoleon <luis.luigis.9999@gmail.com>
2025-04-05 11:58:24 -0600

    Example lab chat-bot

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import lmstudio as lms
 
+def read_context(file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        file_content = f.read()
+    return file_content
+
+    return file_content
 # Import the lmstudio library and alias it as lms
 # Enable asynchronous LLM for better performance
 # Model 1 - Chat completion
@@ -15,10 +21,12 @@ def basic_chat_response(input_text):
 
 
 # Chat completion with context
-def chat_with_context(input_text):
+def chat_with_context(input_text, context):
     # Chat completion with context
     model = lms.llm("llama-3.2-1b-instruct")
-    chat = lms.Chat("Eres un residente de filosofía.")
+    chat = lms.Chat(context)
+    print(input_text)
+    # Add context to the chat
     chat.add_user_message(input_text)
     result = model.respond(chat)
     return result
@@ -58,8 +66,23 @@ def main():
     # print(basic_chat_response("Cuál es el propósito de la vida?"))
 
     # Chat completion with context
-    print(chat_with_context("Cuál es el propósito de la vida?"))
-
+    context = read_context("context.txt")
+    # print(chat_with_context("¿De qué trata el laboratorio de inventores?", context))
+    print()
+    print(chat_with_context("¿Qué se hace en el laboratorio?\n", context))
+    print('-' * 50)
+    print(chat_with_context("¿Qué es el laboratorio de inventores?\n", context))
+    print('-' * 50)
+    print(chat_with_context("¿Cuál es el horario de atención?\n", context))
+    print('-' * 50)
+    print(chat_with_context("¿Puedo hacer mi servicio social en el laboratorio?\n", context))
+    print('-' * 50)
+    print(chat_with_context("Cómo imprimo algo?\n", context))
+    print('-' * 50)
+    print(chat_with_context("Dónde está el lab?\n", context))
+    print('-' * 50)
+    print(chat_with_context("Servicios que ofrece el laboratorio\n", context))
+    print()
     # Model 2
     # Describe image
     # print(describe_image("./lizard.jpg"))
